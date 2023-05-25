62898ae2dc649fe073057e379eea247b9f4e2374
Arshpreet <arshpreets2807@gmail.com>
2023-05-25 23:19:50 +0530

    Episode 2: Complete

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Golang_microservice
 Learning to build microservices using Golang
+
+
+## Concepts
+1. Dependency Injection
+
+This concept basically allows us to put dependency as a variable. For better understanding, lets take an example of the file "handlers/hello.go". Now in this file, we have created a new object called hello which will serve the request coming in "/" path. To create a new handler we created a function called *NewHello*, and we are passing in a logger **l**. Logger basically allows us to log the events. Now it can be the "stdout" or a file maybe, so this can be changed as per our requirements.
--- main.go
+++ main.go
@@ -1,37 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/ArshpreetS/Golang_microservice/handlers"
 )
 
 func main() {
-	// reqeusts to the path /goodbye with be handled by this function
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
 
-	// any other request will be handled by this function
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Running Hello Handler")
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	helloHandler := handlers.NewHello(l)
 
-		// read the body
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Error reading body", err)
-			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
-			return
-		}
+	goodbyeHandler := handlers.NewGoodBye(l)
 
-		// write the response
-		fmt.Fprintf(rw, "Hello %s\n", b)
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", helloHandler)
+	sm.Handle("/goodbye", goodbyeHandler)
+
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
 	// Listen for connections on all ip addresses (0.0.0.0)
 	// port 9090
 	log.Println("Starting Server")
-	err := http.ListenAndServe(":9090", nil)
-	log.Fatal(err)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
